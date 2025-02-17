86efa8b6e7250d1aa35996ebf205da2b84634071
Jacob-DW <85499408+Jacob-DW@users.noreply.github.com>
2025-02-17 11:06:14 +0000

    Add files via upload

--- tutorial.py
+++ tutorial.py
@@ -8,8 +8,30 @@ import tkinter as tk
 def tutorial():
 
     def blackjack():
-        newusertext=tk.Label(tutorial_frame, text="Blackjack is a card game where the goal is to get as close to 21 as possible without going over. Each player, including the dealer, is dealt two cards. Numbered cards are worth their face value, face cards (Jack, Queen, and King) are worth 10, and Aces can count as either 1 or 11, depending on what benefits the player most. Players can choose to 'Twist' (take another card) or 'Stick' (keep their current total). If a player’s total exceeds 21, they bust and lose automatically. After the players finish, the dealer reveals their hidden card and must continue drawing until they reach at least 17. The player wins if they have a higher total than the dealer without busting or if the dealer busts. If both have the same total, the game is a draw. ", font=('Times', 24))
-        newusertext.pack(pady=5)
+
+        blackjack_button.destroy()
+
+        #text to show how toplay blackjack
+        tutorialtext=tk.Label(tutorial_frame, text="Blackjack is a card game where the goal is to get as close to 21 as possible without going over", font=('Times', 24))
+        tutorialtext.pack(pady=5)
+
+        tutorialtext1=tk.Label(tutorial_frame, text="Each player, including the dealer, is dealt two cards", font=('Times', 24))
+        tutorialtext1.pack(pady=5)
+
+        tutorialtext2=tk.Label(tutorial_frame, text="Numbered cards are worth their face value, face cards (Jack, Queen, and King) are worth 10, and Aces can count as either 1 or 11", font=('Times', 24))
+        tutorialtext2.pack(pady=5)
+
+        tutorialtext3=tk.Label(tutorial_frame, text="Players can choose to 'Twist' (take another card) or 'Stick' (keep their current total). ", font=('Times', 24))
+        tutorialtext3.pack(pady=5)
+
+        tutorialtext4=tk.Label(tutorial_frame, text="If a player’s total exceeds 21, they bust and lose automatically. ", font=('Times', 24))
+        tutorialtext4.pack(pady=5)
+
+        tutorialtext5=tk.Label(tutorial_frame, text="After the players finish, the dealer reveals their hidden card and must continue drawing until they reach at least 17. ", font=('Times', 24))
+        tutorialtext5.pack(pady=5)
+
+        tutorialtext6=tk.Label(tutorial_frame, text="The player wins if they have a higher total than the dealer without busting or if the dealer busts. If both have the same total, the game is a draw. ", font=('Times', 24))
+        tutorialtext6.pack(pady=5)
 
 
     tutorial_frame = tk.Toplevel()
